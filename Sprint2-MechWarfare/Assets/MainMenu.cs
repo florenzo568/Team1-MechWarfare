b25f213706d8e73e981e81113d5e58ae1dbc4b9a
SkyCas525 <skylercastilla19@gmail.com>
2022-11-10 11:33:54 -0800

    Menu Works now

--- Sprint2-MechWarfare/Assets/MainMenu.cs
+++ Sprint2-MechWarfare/Assets/MainMenu.cs
@@ -10,8 +10,14 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-  
-
+  public void Meneu()
+    {
+        SceneManager.LoadScene("CreditScene", LoadSceneMode.Single);
+    }
+    public void Credit()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
     public void QuitGame()
     {
         Debug.Log("Quit");
