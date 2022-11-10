f64f16d38de322ed464093de767148fb221c7d51
florenzo568 <florenzo568@gmail.com>
2022-11-10 12:10:06 -0800

    Rockets + health bar

--- Sprint2-MechWarfare/Assets/Scripts/Rockets.cs
+++ Sprint2-MechWarfare/Assets/Scripts/Rockets.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class Rockets : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private Camera mainCam;
+    private Vector3 mousePos;
+    public GameObject rocket;
+    [SerializeField]
+    Movement Player;
+    public Transform bulletTransform;
+    public bool canFire = true;
+    private float Timer;
+    public float FireRate;
+    public int Ammo;
     void Start()
     {
-        
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 rotation = mousePos - transform.position;
+
+        float rotationZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0, 0, rotationZ);
+        if (mousePos.x < 0)
+        {
+            Player.Renderer.flipX = true;
+        }
+        else if (mousePos.x > 0)
+        {
+            Player.Renderer.flipX = false;
+        }
+
+        if (!canFire)
+        {
+            Timer += Time.deltaTime;
+            if (Timer > FireRate)
+            {
+                canFire = true;
+                Timer = 0;
+            }
+        }
+        if (Player.Rockets == true)
+        {
+            if (Input.GetMouseButton(0) && canFire)
+            {
+                canFire = false;
+                Instantiate(rocket, bulletTransform.position, Quaternion.identity);
+                Ammo -= 1;
+            }
+        }
+
     }
 }
+
+
