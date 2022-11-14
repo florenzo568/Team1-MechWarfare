0d31e61ea21b31a7c922438681891cd967ab1c69
florenzo568 <florenzo568@gmail.com>
2022-11-13 19:41:54 -0800

    fixed flamethrower and sniper

--- Sprint2-MechWarfare/Assets/Scripts/FlameThrower.cs
+++ Sprint2-MechWarfare/Assets/Scripts/FlameThrower.cs
@@ -7,7 +7,8 @@ public class FlameThrower : MonoBehaviour
     [SerializeField] Movement Player;
     [SerializeField] SpriteRenderer Sprite;
     [SerializeField] BoxCollider2D Flame;
-
+    public Camera mainCam;
+    private Vector3 mousePos;
     
     void Start()
     {
@@ -18,6 +19,15 @@ public class FlameThrower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+        Vector3 rotation = mousePos - transform.position;
+
+        float rotationZ = Mathf.Atan2(rotation.y,rotation.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0,0, rotationZ);
+
         if(Player.flame == true)
         {
             if(Input.GetMouseButton(0))
