bcae499a4c76827c3c9d2831eeee418394e1b0b8
florenzo568 <florenzo568@gmail.com>
2022-11-14 19:26:06 -0800

    Revert "push test"
    This reverts commit 592c7a48bc3c06278c1a8bea43a57553912d3cb0.

--- Sprint2-MechWarfare/Assets/Scripts/Current Weapon.cs
+++ Sprint2-MechWarfare/Assets/Scripts/Current Weapon.cs
@@ -1,13 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 public class CurrentWeapon : MonoBehaviour
 {
     [SerializeField] Movement Player;
-    [SerializeField] TextMeshProUGUI Weapon;
+    [SerializeField] TMP_Text Weapon;
     void Start()
     {
         Weapon.text = "Balius VI";
