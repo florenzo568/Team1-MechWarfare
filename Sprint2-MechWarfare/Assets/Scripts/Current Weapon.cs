592c7a48bc3c06278c1a8bea43a57553912d3cb0
florenzo568 <florenzo568@gmail.com>
2022-11-14 19:12:50 -0800

    push test

--- Sprint2-MechWarfare/Assets/Scripts/Current Weapon.cs
+++ Sprint2-MechWarfare/Assets/Scripts/Current Weapon.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class CurrentWeapon : MonoBehaviour
 {
     [SerializeField] Movement Player;
-    [SerializeField] TMP_Text Weapon;
+    [SerializeField] TextMeshProUGUI Weapon;
     void Start()
     {
         Weapon.text = "Balius VI";
